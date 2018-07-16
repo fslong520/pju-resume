1047298ee8c356a391d0a4beb9ef381241693482
fslong <fsl470657570@outlook.com>
2018-07-16 11:03:04 +0800

    自动生成pdf文档（存在bug）

--- README.md
+++ README.md
@@ -2,23 +2,41 @@
 An online resume frame by Pyrhon+jQuery+UIkit.  
 这是本人写的一个简历框架（开的新坑），主要是一个动态的类似打字机效果的简历（后续可能会放更多的特效）。  
 
----
+---  
 ## 运行效果 
-![简历效果图初步.gif](https://i.loli.net/2018/07/16/5b4bf047f2e55.gif)<!--删除连接：https://sm.ms/delete/DokAaLfv28dP5VJ--> 
----
+![简历效果图初步.gif](https://i.loli.net/2018/07/16/5b4bf047f2e55.gif)<!--删除连接：https://sm.ms/delete/DokAaLfv28dP5VJ-->    
+
+---  
 ## 使用说明
 1. 克隆仓库到本地；
 2. 修改index.html、indexHeader.html里的title标签为自己的简历；
-3. 在content.html里面编写你自己的简历，静态的就行；
-4. 编写完简历之后执行main.py将自动生成indexStatic.html文件，这个便是静态简历最终版，可以导出成pdf（如果报错可能是相关模块没安装，差什么pip一下就行，自动导出成pdf功能稍后添加）；
+3. 在content.html里面编写你自己的简历，静态的就行(**注意：因为解析网页数据的时候是逐行解析的，所以只需要把想在同一时间显示的代码放在同一行即可。**)；
+4. 编写完简历之后执行main.py将自动生成indexStatic.html文件，这个便是静态简历最终版，同时也导出同名pdf文件（如果报错可能是相关模块没安装，差什么pip一下就行；如果提示导出pdf失败，那就是没有安装pdfkit或者wkhtmltox软件，检查下哪个没安装，如果是pdfkit没安装，用pip安装一下即可，如果是wkhtmltox没安装请访问其官网（ [wkhtmltox官网](https://wkhtmltopdf.org/downloads.html)）安装，如果是在Linux环境下运行或wkhtmltopdf没安装在默认目录，需要进入main.py修改`path_wk = r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'`为你的设备中wkhtmltopdf的安装位置。）；
 5. 在浏览器打开index.html就能看到效果，如果你的简历不是两栏的，需要修改mainHeader.js文件（内容很简单，稍后我会完善里面的注释）；
-6. 如果需要单独修改某条的特效，要么在content.html里面写好，要么在maind.js里单独修改；  
+6. 如果需要单独修改某条的特效，要么在content.html里面写好，要么在maind.js里单独修改；   
+
+---  
+## bug与后续计划
+### bug与不友好的地方
+1. 生成的pdf格式有问题，一些样式、分栏会很奇怪（有些js代码控制的样式没执行）；
+2. 修改简历比较麻烦；
+### 计划
+1. 进一步封装各个方法、类；
+2. 可以使用markdown编辑简历或者文章，然后再生成（感觉这种模式更加适合使用markdown写的文章）；
+3. 未完待续...
+
+---
+*参考文献：*  
+> [1、wkhtmltopdf 中文参数详解](https://blog.csdn.net/u014644418/article/details/51584553)  
+> [2、python--生成pdf](https://www.jianshu.com/p/91fa0420f621)
+
 ---
 ## 开发日志
-### 2018.07.13 
+### 2018.07.16 
 1. 修改主程序；
 2. 自动生成静态简历页；
 3. 自动生成js内容；
+4. 自动生成pdf文档（存在bug）；
 ### 2018.07.13  
 1. 初步完成了很简陋的功能（打字机）；
 2. 简历内容还需要使劲完善。
--- main.py
+++ main.py
@@ -8,6 +8,7 @@ __author__ = 'fslong'
 
 import os
 import re
+import pdfkit
 
 
 def createJScode(htmlstr):
@@ -44,26 +45,62 @@ def createJScode(htmlstr):
     return message+'i+=1; setTimeout(() => {$("#contentDiv").html("%s");}, i*100);\n' % htmlstr
 
 
-with open('content.html', 'r', encoding='utf-8') as f:
-    lines = f.readlines()
-    htmlList = []
-    htmlstr = ''
-    jsCode = ''
-    for line in lines:
-        htmlstr += line.strip()
-        htmlList.append(htmlstr)
-    # 生成静态的简历页面：
-    with open('indexHeader.html', 'r', encoding='utf-8') as f:
-        indexHeader = f.read()
-    with open('indexFooter.html', 'r', encoding='utf-8') as f:
-        indexFooter = f.read()
-    with open('indexStatic.html', 'w', encoding='utf-8') as f:
-        f.write(indexHeader+htmlstr+indexFooter)
-    for htmlstr in htmlList:
-        jsCode += createJScode(htmlstr)
-jsCode = '\n$(function () { \nlet i=40;'+jsCode + \
-    'i+=1; setTimeout(() => {$("#contentbh3").text("简历暂时写完了，谢谢大家！");}, i*100);i+=10; setTimeout(() => {$("#contentb").fadeOut("slow");}, i*100);});'
-with open('js/mainHeader.js', 'r', encoding='utf-8') as f:
-    jsHeader = f.read()
-with open('js/main.js', 'w', encoding='utf-8') as f:
-    f.write(jsHeader+jsCode)
+if __name__ == '__main__':
+    with open('content.html', 'r', encoding='utf-8') as f:
+        lines = f.readlines()
+        htmlList = []
+        htmlstr = ''
+        jsCode = ''
+        for line in lines:
+            htmlstr += line.strip()
+            htmlList.append(htmlstr)
+        # 生成静态的简历页面：
+        with open('indexHeader.html', 'r', encoding='utf-8') as f:
+            indexHeader = f.read()
+        with open('indexFooter.html', 'r', encoding='utf-8') as f:
+            indexFooter = f.read()
+        with open('indexStatic.html', 'w', encoding='utf-8') as f:
+            f.write(indexHeader+htmlstr+indexFooter)
+        for htmlstr in htmlList:
+            jsCode += createJScode(htmlstr)
+    jsCode = '\n$(function () { \nlet i=40;'+jsCode + \
+        'i+=1; setTimeout(() => {$("#contentbh3").text("简历暂时写完了，谢谢大家！");}, i*100);i+=10; setTimeout(() => {$("#contentb").fadeOut("slow");}, i*100);});'
+    with open('js/mainHeader.js', 'r', encoding='utf-8') as f:
+        jsHeader = f.read()
+    with open('js/main.js', 'w', encoding='utf-8') as f:
+        f.write(jsHeader+jsCode)
+    a = input('是否同时生成pdf文件？\n目前还是有些bug，如果你的简历分两栏，会变成一栏，感觉是一些css样式的问题，尚未解决。y/n?\n')
+    if a == 'y':
+        # 转换静态html文件到pdf：
+        # 安装位置，需要修改为你的设备wkhtmltopdf软件安装位置
+        path_wk = r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
+        config = pdfkit.configuration(wkhtmltopdf=path_wk)
+        options = {
+            'page-size': 'A4',  # Letter
+            'minimum-font-size': 25,
+            # 'image-dpi':1500, ###
+            'orientation': 'landscape',### 横向，注释掉就是纵向，详情请看：https://blog.csdn.net/u014644418/article/details/51584553
+
+            'margin-top': '0.1in',  # 0.75in
+            'margin-right': '0.1in',
+            'margin-bottom': '0.1in',
+            'margin-left': '0.1in',
+            'encoding': 'UTF-8',  # 支持中文
+            'custom-header': [
+                ('Accept-Encoding', 'gzip')
+            ],
+            'cookie': [
+                ('cookie-name1', 'cookie-value1'),
+                ('cookie-name2', 'cookie-value2'),
+            ],
+            'outline-depth': 10,
+        }
+        try:
+            path = os.path.join(os.path.abspath('.'), 'indexStatic.html')
+            print(path)
+            pdfkit.from_file(path, 'indexStatic.pdf',
+                             configuration=config, options=options)
+        except:
+            print('转换失败，请查看相关代码问题。')
+    else:
+        print('转换完毕！')
