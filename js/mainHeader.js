d07f244a0e2edce40ddea4c6d7c6971c45a1e137
fslong <fsl470657570@outlook.com>
2018-07-16 09:00:18 +0800

    修改主程序

---
+++ js/mainHeader.js
@@ -0,0 +1,43 @@
+function showElementsByWord(selector, char) {
+    var char1 = char;
+    var char2 = "";
+    var char1Arr = char1.split('');
+    var char1Arr2 = new Array()
+    var i = 0
+
+    $.each(char1Arr, function (index, j) {
+        char2 += j;
+        i += 1;
+        char1Arr2[i] = char2;
+    });
+    for (i = 1; i < char1Arr2.length; i++) {
+        a = char1Arr2[i];
+        setH3(selector, i);
+    }
+
+    function setH3(selector, i) {
+        setTimeout(function () {
+            selector.html(char1Arr2[i]);
+        }, i * 99);
+    }
+}
+
+function showElementsByElement(selector, char, i) {
+    setTimeout(function () {
+        selector.html(char);
+    }, i * 1000);
+}
+$(function () {
+    showElementsByWord($("#contentbh3"), "今天我们来写一封简历！");
+    showElementsByWord($("#contentlh3"), "这里将会展示个人基本信息。");
+    showElementsByWord($("#contentrh3"), "这里将会展示个人简历详情。");
+
+});
+$(function () {
+    setTimeout(() => {
+        showElementsByWord($("#contentbh3"), "这里将会显示程序执行日志，就像控制台一样。");
+    }, 1200);
+    setTimeout(() => {
+        showElementsByWord($("#contentbh3"), "那么我们开始吧!");
+    }, 3500);
+});
--- main.py
+++ main.py
@@ -22,14 +22,14 @@ def createJScode(htmlstr):
             elementsListLeft.append(i)
     for i in elementsListLeft:
         if i.startswith('<!--'):
-            message=i.split('--')[1]
+            message = i.split('--')[1]
     elementsListLeft = elementsListLeft[::-1]
     if len(elementsListLeft) > len(elementsListRight):
         i = len(elementsListRight)
         while i < len(elementsListLeft):
             j = elementsListLeft[i]
             if j.startswith('<img')or j.startswith('<input') or j.startswith('<br') or j.startswith('<hr') or j.startswith('<!--'):
-                pass 
+                pass
             else:
                 if re.match(r'(.*?)(\s)', elementsListLeft[i]):
                     htmlstr += '</' + \
@@ -38,10 +38,10 @@ def createJScode(htmlstr):
                     htmlstr += '</' + \
                         elementsListLeft[i].split(' ')[0][1:]
             i += 1
-    message = 'i+=1; setTimeout(() => {$("#contentbh3").text("%s");}, i*100);' % message
+    message = 'i+=1; setTimeout(() => {$("#contentbh3").text("%s");}, i*100);\n' % message
     htmlstr = htmlstr.replace('\"', '\'')
 
-    return message+'i+=1; setTimeout(() => {$("#contentDiv").html("%s");}, i*100);' % htmlstr
+    return message+'i+=1; setTimeout(() => {$("#contentDiv").html("%s");}, i*100);\n' % htmlstr
 
 
 with open('content.html', 'r', encoding='utf-8') as f:
@@ -51,10 +51,23 @@ with open('content.html', 'r', encoding='utf-8') as f:
     jsCode = ''
     for line in lines:
         htmlstr += line.strip()
-        htmlList.append(htmlstr)
+        # 由于<progress标签的问题，会出现奇怪的事情，所以，progress标签不进行打字输出：
+        if line.strip().startswith('<progress'):
+            pass
+        else:
+            htmlList.append(htmlstr)
+    # 生成静态的简历页面：
+    with open('indexHeader.html', 'r', encoding='utf-8') as f:
+        indexHeader = f.read()
+    with open('indexFooter.html', 'r', encoding='utf-8') as f:
+        indexFooter = f.read()
+    with open('indexStatic.html', 'w', encoding='utf-8') as f:
+        f.write(indexHeader+htmlstr+indexFooter)
     for htmlstr in htmlList:
         jsCode += createJScode(htmlstr)
-jsCode = '\n$(function () { let i=40;'+jsCode + \
+jsCode = '\n$(function () { \nlet i=40;'+jsCode + \
     'i+=1; setTimeout(() => {$("#contentbh3").text("简历暂时写完了，谢谢大家！");}, i*100);i+=10; setTimeout(() => {$("#contentb").fadeOut("slow");}, i*100);});'
-with open('js/main.js', 'a', encoding='utf-8') as f:
-    f.write(jsCode)
+with open('js/mainHeader.js', 'r', encoding='utf-8') as f:
+    jsHeader = f.read()
+with open('js/main.js', 'w', encoding='utf-8') as f:
+    f.write(jsHeader+jsCode)
